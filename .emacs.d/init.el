c18e234ef12beb6069999db9386f0581ec776ac9
Zhiguang Li <muzili@gmail.com>
2018-03-17 16:08:36 +0800

    Add emacs dot files
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

---
+++ .emacs.d/init.el
@@ -0,0 +1,16 @@
+(package-initialize)
+(org-babel-load-file "~/.emacs.d/start.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yasnippet-snippets yaml-mode xml-rpc whitespace-cleanup-mode web-mode use-package tidy tide smtpmail-multi smart-tabs-mode skype simplenote2 simpleclip racer plantuml-mode paredit pandoc-mode ox-pandoc org-vcard org-plus-contrib org-pandoc org-octopress org-jekyll org-gnome org-gcal org-caldav nodejs-repl markdown-preview-mode magit lsp-vue lsp-rust lsp-python lsp-javascript-typescript lsp-java js2-mode jdee javadoc-help irony html-to-markdown helm-zhihu-daily helm-orgcard helm-notmuch helm-mu helm-gtags helm-go-package helm-git helm-descbinds helm-company helm-ag-r helm-ag gradle-mode google-c-style golint go-snippets go-projectile go-play go-errcheck go-direx go-autocomplete exec-path-from-shell evil-mu4e emmet-mode edit-server ecb dtrt-indent dockerfile-mode darcula-theme cquery company-quickhelp company-lsp company-go cargo calfw-gcal calfw c-eldoc boxquote benchmark-init bbdb autopair auto-complete-clang-async auctex ansible-doc ansible ac-helm ac-c-headers))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button))))))
