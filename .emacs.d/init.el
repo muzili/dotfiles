d9ee1dfeefd09e7a1a7f08a48a38dd42e797b9d0
Zhiguang Li <muzili@gmail.com>
2018-07-27 21:35:13 +0800

    optimize the configurations
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,2 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/start.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (rust-mode go-guru godoctor go-eldoc yasnippet-snippets whitespace-cleanup-mode web-mode use-package tide projectile nodejs-repl markdown-preview-mode magit lsp-vue lsp-ui helm-mu helm-flx git-timemachine git-gutter exec-path-from-shell emmet-mode dockerfile-mode company-lsp company-irony company-go company-anaconda benchmark-init))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button)))) t))
