338c37e82a13acd70532df95461dcc199f7380bb
Li Zhiguang <lizhiguang@ainirobot.com>
2019-01-19 19:08:03 +0800

    a lot of stuff redesigned
    Signed-off-by: Li Zhiguang <lizhiguang@ainirobot.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,5 +2,6 @@
 (org-babel-load-file "~/.emacs.d/start.org")
 
 ;http://emacsblog.org/2008/12/06/quick-tip-detaching-the-custom-file/
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file 'noerror)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file 'noerror))
