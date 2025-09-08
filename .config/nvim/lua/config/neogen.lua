1c50181b5ede8ad6b7c6edee305ecde0c037cd9e
Zhiguang Li <muzili@gmail.com>
2025-09-08 22:00:45 +0800

    remove old configs
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

--- .config/nvim/lua/config/neogen.lua
+++
@@ -1,8 +0,0 @@
-require('neogen').setup({
-    enabled = true,
-    input_after_comment = true,
-})
-
-opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<space>cg", ":lua require('neogen').generate()<CR>", opts)
-
