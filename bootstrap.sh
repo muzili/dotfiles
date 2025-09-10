3d3f9400bf7165165f3da3b4614876c7ef4da38e
Zhiguang Li <muzili@gmail.com>
2025-09-10 11:33:20 +0800

    update mirror url
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,13 @@ set -e
 
 mkdir -p "$HOME/.local/bin"
 
+git config --global url."https://xiu.lzg.cc/gh/".insteadOf "https://github.com/"
+git config --global url."https://xiu.lzg.cc/gl/".insteadOf "https://gitlab.com/"
+git config --global url."https://xiu.lzg.cc/gitea/".insteadOf "https://gitea.com/"
+git config --global url."https://xiu.lzg.cc/codeberg/".insteadOf "https://codeberg.org/"
+git config --global url."https://xiu.lzg.cc/sf/".insteadOf "https://sourceforge.net/"
+git config --global url."https://xiu.lzg.cc/aosp/".insteadOf "https://android.googlesource.com/"
+
 echo "🚀 Installing chezmoi..."
 curl https://xiu.lzg.cc/gh/twpayne/chezmoi/releases/download/v2.65.1/chezmoi-linux-amd64-musl -o $HOME/.local/bin/chezmoi
 chmod +x $HOME/.local/bin/chezmoi
