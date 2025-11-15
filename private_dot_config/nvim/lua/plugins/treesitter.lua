97f874d18ec4aa0c031607a4c842ae0670df8b4b
Zhiguang Li <muzili@gmail.com>
2025-11-15 09:19:15 +0800

    disable jsonc
    Signed-off-by: Zhiguang Li <muzili@gmail.com>

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,16 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+  opts = function(_, opts)
+    -- 从 ensure_installed 删除 jsonc
+    if type(opts.ensure_installed) == "table" then
+      opts.ensure_installed = vim.tbl_filter(function(lang)
+        return lang ~= "jsonc"
+      end, opts.ensure_installed)
+    end
+
+    -- 显式禁用 jsonc 安装
+    opts.ignore_install = opts.ignore_install or {}
+    table.insert(opts.ignore_install, "jsonc")
+  end,
 }
